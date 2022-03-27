88f38c36ad1fd59a9837581f00a9209581efd4cc
ihebmbarki <45261121+ihebmbarki@users.noreply.github.com>
2022-03-27 13:36:18 +0100

    predict-disease

--- Controller/FileRouter.py
+++ Controller/FileRouter.py
@@ -19,14 +19,18 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 @router.post("/predict_disease/")
+#asyc call (the call isn't blocked until you get the result)
 async def create_upload_file(file: UploadFile):
     file.filename = f"{uuid.uuid4()}.jpg"
-    contents = await file.read()  # <-- Important!
-    IMAGEDIR = "Service/"
+    #the read function is defined with async, call it with await
+    contents = await file.read()
+    IMAGEDIR = "files/"
+
+    #save file to local path (db)
     with open(f"{IMAGEDIR}{file.filename}", "wb") as f:
         f.write(contents)
 
 
-    results = fileService.prediction_results(contents, file.filename)
+    results = fileService.prediction_results( file.filename)
     return results
 
--- Service/FileService.py
+++ Service/FileService.py
@@ -1,22 +1,29 @@
-# MLP for Pima Indians Dataset Serialize to JSON and HDF5
-import json
+#TensorFlow trains and runs deep neural networks for handwritten digit classification
+# image recognition in this case
 import tensorflow as tf
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.models import model_from_json
-import numpy
-import os
+# The Python Imaging Library adds image processing capabilities to your Python interpreter.
 from PIL import Image
-
+#  Keras Reduces cognitive load: it offers consistent & simple APIs
+#  minimizes the number of user actions required for common use cases
+#  clear & actionable error messages.
+from keras.models import model_from_json
+#serum is a fresh take on Dependency Injection in Python 3.
 from serum import dependency, inject
 
+
+# resize and preprocess image
+# load machine learning model
+# save model pickle
+# input the image and predict
+# return the results in form of json
+# load json and create model
 
 @dependency
 @inject
 class FileService:
     def __init(self):
         pass
-    def prediction_results(self, image, filename):
+    def prediction_results(self, filename):
         # resize and preprocess image
         # load machine learning model
         # save model pickle
@@ -24,16 +31,22 @@ class FileService:
         # return the results in form of json
         # load json and create model
         labels = {1:'Bulging_Eyes', 2:'Crossed_Eyes', 3:'Glaucoma', 4:'Cataracts', 5:'Uveitis'}
-        image = Image.open(f"Service/{filename}")
+        image = Image.open(f"files/{filename}")
+        #resize image by given settings in the model
         image = tf.image.resize(image, (32, 32))
+        #load model architecture from model.json
         with open('Service/model.json', 'r') as f:
-            loaded_model_json = f.read()
-        loaded_model = model_from_json(loaded_model_json)
-        # load weights into new model
+            model_architecture = f.read()
+        loaded_model = model_from_json(model_architecture)
+        # load weights into the model architecture
         loaded_model.load_weights("Service/model.h5")
-        # evaluate loaded model on test data
+        # define loss func and optimizer
+        # optimiser searchs for the minimun of the loss func when doing back propagation
         loaded_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         results = {}
-        pr = loaded_model.predict(image[None])
-        results['disease'] = labels[pr.argmax(axis=1)[0]]
+        print(image[None].shape)
+        #predict expects shape=(None, 32, 32, 3)
+        prediction = loaded_model.predict(image[None])
+        #return thee maximum probability value from 5 categories using argmax
+        results['disease'] = labels[prediction.argmax(axis=1)[0]]
         return results
